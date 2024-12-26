d43bffc6533f13a56191ba313b6233219b8ae8af
Master.G <master-g@users.noreply.github.com>
2024-12-26 18:36:23 +0800

    feat: demo

--- samples/main.c
+++ samples/main.c
@@ -1,7 +1,79 @@
+#include <macaron/macaron.h>
 #include <stdio.h>
+#include <sys/time.h>
+
+#define FRAME_DURATION (1/60.0)
+#define WORKER_COUNT 4
+#define SUB_STEPS 4
+
+#define NUM_OF_PUCKS 19
+
+typedef enum PuckColor
+{
+    PuckColor_White,
+    PuckColor_Black,
+    PuckColor_Red,
+} PuckColor;
+
+#define PUCK_R 0.16f
+
+typedef struct
+{
+    b2BodyId bodyId;
+    PuckColor color;
+} Puck;
+
+Puck Puck_Spawn(const b2WorldId worldId, const PuckColor color, const b2Vec2 position, void* userData)
+{
+    b2BodyDef bodyDef = b2DefaultBodyDef();
+    bodyDef.type = b2_dynamicBody;
+    bodyDef.isBullet = true;
+    bodyDef.linearDamping = 1.8f;
+    bodyDef.angularDamping = 2.0f;
+    bodyDef.position = position;
+    bodyDef.userData = userData;
+
+    const b2BodyId bodyId = b2CreateBody(worldId, &bodyDef);
+
+    b2ShapeDef shapeDef = b2DefaultShapeDef();
+    shapeDef.friction = 0.0f;
+    shapeDef.restitution = 1.0f;
+    shapeDef.density = 0.45f;
+
+    const Puck puck = { bodyId, color };
+
+    return puck;
+}
+
+void Puck_DeSpawn(Puck *puck)
+{
+    b2DestroyBody(puck->bodyId);
+    puck->bodyId = b2_nullBodyId;
+}
 
 int main() {
-  printf("Hello World\n");
+    const int steps = 10000;
+
+    b2WorldDef worldDef = b2DefaultWorldDef();
+    worldDef.gravity = b2Vec2_zero;
+    worldDef.enableContinous = true;
+    worldDef.workerCount = WORKER_COUNT;
+    worldDef.enableSleep = false;
+
+    const b2WorldId worldId = b2CreateWorld(&worldDef);
+
+    const time_t now = time(NULL);
+
+    for (int i = 0; i < steps; i++)
+    {
+        b2World_Step(worldId, FRAME_DURATION, SUB_STEPS);
+    }
+
+    const time_t elapsed_ms = (time(NULL) - now) * 1000;
+
+    printf("Elapsed time: %ld ms\n", elapsed_ms);
+
+    b2DestroyWorld(worldId);
 
   return 0;
  }
