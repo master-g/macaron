45f2300913ce7db092f9cee5f3c8051da7e2781d
Master.G <master-g@users.noreply.github.com>
2024-12-31 11:26:43 +0800

    feat: default puck placement

--- src/defaults.c
+++ src/defaults.c
@@ -1,5 +1,7 @@
 #include <macaron/types.h>
 
+#define SPI3 0.8660254037844386f
+
 CarromWorldDef CarromDefaultWorldDef(void)
 {
 	CarromWorldDef def = {0};
@@ -11,7 +13,6 @@ CarromWorldDef CarromDefaultWorldDef(void)
 	def.frameDuration = 1.0f / 60;
 	return def;
 }
-
 
 CarromPuckPhysicsDef CarromDefaultPuckPhysicsDef(void)
 {
@@ -62,7 +63,36 @@ CarromGameDef CarromDefaultGameDef(void)
 	def.pocketDef = CarromDefaultPocketDef();
 	def.strikerLimitDef = CarromDefaultStrikerLimitDef();
 	def.numOfPucks = 19;
-	// TODO: place these pucks
+
+	const float outR = def.puckPhysicsDef.radius + 0.012f;
+	const CarromPuckPositionDef posDefs[] = {
+		{1, PuckColor_Red, {0.0f, 0.0f}},
+
+		{2, PuckColor_White, {0.0f, outR * 4}},
+		{3, PuckColor_White, {0.0f, -outR * 2}},
+		{4, PuckColor_White, {0.0f, -outR * 4}},
+		{5, PuckColor_White, {-outR * 2 * SPI3, outR * 2 * 0.5f}},
+		{6, PuckColor_White, {-outR * 4 * SPI3, outR * 4 * 0.5f}},
+		{7, PuckColor_White, {outR * 2 * SPI3, outR * 2 * 0.5f}},
+		{8, PuckColor_White, {outR * 4 * SPI3, outR * 4 * 0.5f}},
+		{9, PuckColor_White, {-outR * 4 * SPI3, -outR * 4 * 0.5f}},
+		{10, PuckColor_White, {outR * 4 * SPI3, -outR * 4 * 0.5f}},
+
+		{11, PuckColor_Black, {0.0f, outR * 2}},
+		{12, PuckColor_Black, {-outR * 4 * SPI3, 0.0f}},
+		{13, PuckColor_Black, {outR * 4 * SPI3, 0.0f}},
+		{14, PuckColor_Black, {-outR * 2 * SPI3, outR * 2 * 0.5f + outR * 2}},
+		{15, PuckColor_Black, {-outR * 2 * SPI3, -outR * 2 * 0.5f}},
+		{16, PuckColor_Black, {-outR * 2 * SPI3, -outR * 2 * 0.5f - outR * 2}},
+		{17, PuckColor_Black, {outR * 2 * SPI3, -outR * 2 * 0.5f}},
+		{18, PuckColor_Black, {outR * 2 * SPI3, -outR * 2 * 0.5f - outR * 2}},
+		{19, PuckColor_Black, {outR * 2 * SPI3, outR * 2 * 0.5f + outR * 2}},
+	};
+
+	for (int i = 0; i < 19; i++)
+	{
+		def.pucksPositions[i] = posDefs[i];
+	}
 
 	return def;
 }
